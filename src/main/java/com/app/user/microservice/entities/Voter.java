460dcba9664954af00bd22443b8379fcf287f6fb
Moises Molina <w7moises@gmail.com>
2022-05-27 13:16:30 -0500

    voter entity added

---
+++ src/main/java/com/app/user/microservice/entities/Voter.java
@@ -0,0 +1,33 @@
+package com.app.user.microservice.entities;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Document(collection = "voter")
+@Data
+public class Voter {
+    @Id
+    private String id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    @Field(name = "last_name")
+    private String lastName;
+    @Email
+    private String email;
+    @Size(min = 8,max =8)
+    private String dni;
+    private Boolean gender;
+    private String age;
+    @Field(name = "is_active")
+    private Boolean isActive;
+    @Field(name = "finger_print")
+    private String fingerprint;
+}
