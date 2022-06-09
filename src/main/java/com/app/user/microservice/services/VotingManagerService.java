7ef17507e64d7332116b7288c79e31aea237b80f
Moises Molina <w7moises@gmail.com>
2022-06-09 08:52:54 -0500

    manager service added

---
+++ src/main/java/com/app/user/microservice/services/VotingManagerService.java
@@ -0,0 +1,12 @@
+package com.app.user.microservice.services;
+
+import com.app.user.microservice.entities.VotingManager;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface VotingManagerService {
+    Flux<VotingManager> findAll();
+    Mono<VotingManager> findById(String id);
+    Mono<VotingManager> save();
+    Mono<VotingManager> update(VotingManager manager,String id);
+}
