80dea88d8b7e89da53f938aa3d0d181faf9ab958
Moises Molina <w7moises@gmail.com>
2022-06-09 08:47:11 -0500

    manager entity added

---
+++ src/main/java/com/app/user/microservice/entities/VotingManager.java
@@ -0,0 +1,29 @@
+package com.app.user.microservice.entities;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Document(collection = "voting_manager")
+@Data
+public class VotingManager {
+    @Id
+    private String id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    @Field(name = "last_name")
+    private String lastName;
+    @Email
+    private String email;
+    @Size(min = 8,max =8)
+    private String dni;
+    private String gender;
+}
